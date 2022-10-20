882a1c5f39d0162be3ff7aaec3107a4a90c4c0b1
DavidCrzBlt <cruzdavid158@gmail.com>
2022-10-20 18:19:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -183,8 +183,8 @@ def empleados():
 
 @app.route('/asistencias', methods=["GET", "POST"])
 def asistencias():
-    today = datetime.now().date()-timedelta(days=1)
-    yesterday = today
+    today = datetime.now().date()
+    yesterday = today-timedelta(days=1)
     proyectos = Proyecto.query.filter_by(status=True)
 
     if request.method == "POST":
